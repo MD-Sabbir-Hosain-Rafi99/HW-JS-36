0e2f4634caa23075f3bbee65df4358c8862a8f75
MD-Sabbir-Hosain-Rafi99 <sabbirhossain.rafi121@gmail.com>
2025-05-14 02:31:16 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let heading = document.getElementById('heading');
+let textInputOne = document.getElementById('textInputOne');
+let btnOne = document.getElementById('btnOne');
+
+btnOne.addEventListener('click', () => {
+    console.log(textInputOne.innerHTML);
+})
